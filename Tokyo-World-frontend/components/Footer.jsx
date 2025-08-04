724c91a4bbae611c966a0cbcef681baaa66d8f7a
AlexLosciale <losciale17@gmail.com>
2025-08-04 16:32:48 +0200

    add:footer

--- Tokyo-World-frontend/components/Footer.jsx
+++ Tokyo-World-frontend/components/Footer.jsx
@@ -1,15 +1,48 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons';
+
 export default function Footer() {
-    return (
-        <footer className="footer bg-dark text-white">
-            <div className="container text-center">
-                <p>&copy; {new Date().getFullYear()} Tokyo World.</p>
-                <p>Follow us on social media!</p>
-                <ul className="social-links list-inline mb-0">
-                    <li><a href="https://twitter.com/tokyoworld">Twitter</a></li>
-                    <li><a href="https://instagram.com/tokyoworld">Instagram</a></li>
-                    <li><a href="https://facebook.com/tokyoworld">Facebook</a></li>
-                </ul>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="footer bg-dark text-white py-4">
+      <div className="container text-center">
+        <p className="mb-2">&copy; {new Date().getFullYear()} Tokyo World.</p>
+        <p className="mb-3">Follow us on social media!</p>
+        <ul className="list-inline mb-0">
+          <li className="list-inline-item mx-2">
+            <a
+              href="https://twitter.com/tokyoworld"
+              className="text-white text-decoration-none"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Twitter"
+            >
+              <FontAwesomeIcon icon={faTwitter} size="2x" />
+            </a>
+          </li>
+          <li className="list-inline-item mx-2">
+            <a
+              href="https://instagram.com/tokyoworld"
+              className="text-white text-decoration-none"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Instagram"
+            >
+              <FontAwesomeIcon icon={faInstagram} size="2x" />
+            </a>
+          </li>
+          <li className="list-inline-item mx-2">
+            <a
+              href="https://facebook.com/tokyoworld"
+              className="text-white text-decoration-none"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Facebook"
+            >
+              <FontAwesomeIcon icon={faFacebook} size="2x" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
